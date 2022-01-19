e60836e99e0d6d8922b2872a91957dddc2fc9cc9
fluke006 <johongirovdiyorbek08@gmail.com>
2022-01-19 23:31:24 +0500

    add

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:7777");
+const socket = io("https://chating-1.herokuapp.com/");
 
 const form = document.querySelector('.form')
 const input = document.querySelector('.input')
